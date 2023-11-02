0eafbc19a028644fdf4c03840a4598ee74c7067a
You Jeong Jang <sunny819819@naver.com>
2023-11-02 15:14:00 +0900

    Create Solution.java

---
+++ 2023 Kakao Blind Recruitment/개인정보수집유효기간/Solution.java
@@ -0,0 +1,71 @@
+import java.util.Vector;
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+         Vector<Integer> vector = new Vector<Integer>();
+
+        //[0] today 년, 월, 일 분리하기
+        int t_year = (today.charAt(0)-'0')*1000 + (today.charAt(1)-'0')*100 + (today.charAt(2)-'0')*10 + (today.charAt(3)-'0');
+        int t_month = (today.charAt(5)-'0')*10 + (today.charAt(6)-'0');
+        int t_day = (today.charAt(8)-'0')*10 + (today.charAt(9)-'0');
+
+
+        for(int index = 0; index<privacies.length; index++){
+            String p = privacies[index];
+
+            int left_months = 0; //유효 기간
+
+            // [1] terms에서 개인정보 약관 종류 찾아서 유효기간 가져오기
+            for(String t : terms ) {
+                double digit = 0;
+                if (t.charAt(0) == p.charAt(p.length() - 1)) {
+                    for (int i = t.length()-1; ; i--) {
+                        if(t.charAt(i)==' ')break;
+                        else{
+                            double num = (t.charAt(i) - '0') * Math.pow(10, digit);
+                            left_months += (int) num;
+                            digit++;
+                        }
+                    }
+                    break;
+                }
+            }
+
+            //[2]: privacies 년 / 월 / 일 각각 분리하기
+            int p_year = (p.charAt(0)-'0')*1000 + (p.charAt(1)-'0')*100 + (p.charAt(2)-'0')*10 + (p.charAt(3)-'0');
+            int p_month =  (p.charAt(5)-'0')*10 + (p.charAt(6)-'0');
+            int p_day = (p.charAt(8)-'0')*10 + (p.charAt(9)-'0');
+
+            //[3]: left_months를 가지고 deadline 계산하기
+            p_year+=left_months/12;
+            p_month+=left_months%12;
+            
+            if(p_month>12) {
+                p_year+=p_month/12;
+                p_month%=12;
+            }
+
+
+            //[4]: 오늘 날짜와 비교해서 유효기간 지났는지 확인
+            // [4]-1 연도가 이미 지났을 경우
+            if(t_year>p_year){
+                vector.add(index+1);
+            }
+            else if(t_year==p_year){
+                //[4]-2 같은 연도에서 유효기간 달이 지났을 경우
+                if(t_month>p_month) {
+                    vector.add(index+1);
+                }
+                //[4]-3 같은 연도, 같은 달에서 하루 이상 지났을 경우
+                else if(t_month==p_month && t_day>=p_day){
+                    vector.add(index+1);
+                }
+            }
+        }
+
+        int[] answer = new int[vector.size()];
+        for(int i =0; i<vector.size(); i++){
+            answer[i]=vector.elementAt(i);
+        }
+        return answer;
+    }
+}

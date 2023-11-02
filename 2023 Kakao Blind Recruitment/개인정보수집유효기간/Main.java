c9beecb69018e27898eebfb8b6467839569c587b
You Jeong Jang <sunny819819@naver.com>
2023-11-02 15:13:37 +0900

    Create Main.java

---
+++ 2023 Kakao Blind Recruitment/개인정보수집유효기간/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String today = "2020.01.01";
+        String[] terms = {"Z 3", "D 5"};
+        String[] privacies = {"2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"};
+
+        System.out.println(Arrays.toString(Solution.solution(today, terms, privacies)));
+    }
+}
